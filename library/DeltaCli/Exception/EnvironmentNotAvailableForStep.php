aad9ca9096a0eaf84aae50cf555c711d2129bb03
Brad Griffith <bgriffith@deltasys.com>
2016-05-04 13:07:23 -0500

    A couple new exception classes.

---
+++ library/DeltaCli/Exception/EnvironmentNotAvailableForStep.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DeltaCli\Exception;
+
+use Exception;
+
+class EnvironmentNotAvailableForStep extends Exception
+{
+
+}
---
+++ library/DeltaCli/Exception/SetterNotPresentForScriptOption.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DeltaCli\Exception;
+
+use Exception;
+
+class SetterNotPresentForScriptOption extends Exception
+{
+    
+}
