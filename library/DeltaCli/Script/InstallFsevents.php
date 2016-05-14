9984757cfb0db22c3b4031680838c5cab03102b7
Brad Griffith <bgriffith@deltasys.com>
2016-05-13 20:29:18 -0500

    Install autoconf for fsevents, if needed.  Tie up some other loose ends in fsevents installation.

--- library/DeltaCli/Script/InstallFsevents.php
+++ library/DeltaCli/Script/InstallFsevents.php
@@ -2,6 +2,7 @@
 
 namespace DeltaCli\Script;
 
+use DeltaCli\Exec;
 use DeltaCli\Project;
 use DeltaCli\Script;
 use Exception;
@@ -19,6 +20,14 @@ class InstallFsevents extends Script
 
     protected function addSteps()
     {
+        exec('autoconf', $output, $exitStatus);
+
+        $autoconfInstalled = true;
+
+        // 127 is "command not found"
+        if (127 === $exitStatus) {
+            $autoconfInstalled = false;
+        }
 
         $this
             ->addStep(
@@ -40,7 +49,60 @@ class InstallFsevents extends Script
             ->addStep(
                 'check-for-phpize',
                 function () {
-                    exec('xcode-select --install');
+                    if (!file_exists('/usr/bin/phpize') || !is_executable('/usr/bin/phpize')) {
+                        throw new Exception(
+                            'phpize is not available.  Run `xcode-select --install` and click the install button.'
+                        );
+                    }
+                }
+            )
+            ->addStep(
+                'check-for-make',
+                function () {
+                    exec('make -v', $output, $exitStatus);
+
+                    if ($exitStatus) {
+                        throw new Exception(
+                            'make is not available.  Run `xcode-select --install` and click the install button.'
+                        );
+                    }
+                }
+            )
+            ->addStep(
+                'delete-tmp-autoconf-folder-if-present',
+                function () {
+                    exec('rm -rf /tmp/autoconf*');
+                }
+            )
+            ->addStep(
+                'download-autoconf',
+                $this->autoconfStep(
+                    'curl -O http://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz',
+                    $autoconfInstalled
+                )
+            )
+            ->addStep(
+                'extract-autoconf',
+                $this->autoconfStep('tar xvfz autoconf-latest.tar.gz', $autoconfInstalled)
+            )
+            ->addStep(
+                'configure-autoconf',
+                $this->autoconfStep('cd autoconf* && ./configure --prefix=/usr/local', $autoconfInstalled)
+            )
+            ->addStep(
+                'make-autoconf',
+                $this->autoconfStep('cd autoconf* && make', $autoconfInstalled)
+            )
+            ->addStep(
+                'install-autoconf',
+                $this->autoconfStep('cd autoconf* && make install', $autoconfInstalled)
+            )
+            ->addStep(
+                'delete-tmp-folder-if-present',
+                function () {
+                    if (file_exists('/tmp/php-pecl-fsevents')) {
+                        exec('rm -rf /tmp/php-pecl-fsevents');
+                    }
                 }
             )
             ->addStep(
@@ -49,7 +111,7 @@ class InstallFsevents extends Script
             )
             ->addStep(
                 'run-phpize',
-                'cd /tmp/php-pecl-fsevents && phpize'
+                'export PHP_AUTOCONF=/usr/local/bin/autoconf; cd /tmp/php-pecl-fsevents && phpize'
             )
             ->addStep(
                 'run-configure',
@@ -69,7 +131,8 @@ class InstallFsevents extends Script
             )
             ->addStep(
                 'add-to-phpdotini',
-                'echo "extension=/usr/local/php/fsevents.so" >> /etc/php.ini'
+                'grep ^extension /etc/php.ini | grep fsevents.so || '
+                . 'echo "extension=/usr/local/php/fsevents.so" >> /etc/php.ini'
             )
             ->addStep(
                 'cleanup',
@@ -85,5 +148,21 @@ class InstallFsevents extends Script
                     }
                 }
             );
+    }
+
+    private function autoconfStep($command, $autoconfInstalled)
+    {
+        return function () use ($command, $autoconfInstalled) {
+            if ($autoconfInstalled) {
+                echo 'autoconf is already installed';
+                return;
+            } else {
+                Exec::run(sprintf('cd /tmp && %s', $command), $output, $exitStatus);
+
+                if ($exitStatus) {
+                    throw new Exception("Failed to install autoconf while running: {$command}");
+                }
+            }
+        };
     }
 }
