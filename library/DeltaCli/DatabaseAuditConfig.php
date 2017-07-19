d2ef3356f29480007c3e3fba0181a35056f038c2
Brad Griffith <griffbrad@gmail.com>
2017-07-19 12:37:44 -0500

    Add ability to exclude WP, Dewdrop and DeltaZend tables from audit.

--- library/DeltaCli/DatabaseAuditConfig.php
+++ library/DeltaCli/DatabaseAuditConfig.php
@@ -16,6 +16,26 @@ class DatabaseAuditConfig
      */
     private $excludedTables = [];
 
+    /**
+     * @var bool
+     */
+    private $excludeDeltaZendTables = true;
+
+    /**
+     * @var bool
+     */
+    private $excludeDewdropTables = true;
+
+    /**
+     * @var bool
+     */
+    private $excludeWordPressCoreTables = false;
+
+    /**
+     * @var string
+     */
+    private $wordPressPrefix;
+
     public function applyDatabaseSpecificConfiguration(callable $configurationCallback)
     {
         $this->databaseSpecificConfigurationCallbacks[] = $configurationCallback;
@@ -43,6 +63,103 @@ class DatabaseAuditConfig
 
     public function tableIsExcluded($table)
     {
+        if ($this->excludeWordPressCoreTables && $this->isWordPressCoreTable($table)) {
+            return true;
+        }
+
+        if ($this->excludeDeltaZendTables && 0 === strpos($table, 'delta_')) {
+            return true;
+        }
+
+        if ($this->excludeDewdropTables && 0 === strpos($table, 'dewdrop_')) {
+            return true;
+        }
+
         return in_array($table, $this->excludedTables);
+    }
+
+    public function setWordPressPrefix($wordPressPrefix)
+    {
+        $this->wordPressPrefix = $wordPressPrefix;
+
+        return $this;
+    }
+
+    public function excludeWordPressCoreTables($prefix)
+    {
+        $this->excludeWordPressCoreTables = true;
+
+        $this->wordPressPrefix = $prefix;
+
+        return $this;
+    }
+
+    public function includeWordPressCoreTables()
+    {
+        $this->excludeWordPressCoreTables = false;
+
+        return $this;
+    }
+
+    public function excludeDeltaZendTables()
+    {
+        $this->excludeDeltaZendTables = true;
+
+        return $this;
+    }
+
+    public function includeDeltaZendTables()
+    {
+        $this->excludeDeltaZendTables = false;
+
+        return $this;
+    }
+
+    public function excludeDewdropTables()
+    {
+        $this->excludeDewdropTables = true;
+
+        return $this;
+    }
+
+    public function includeDewdropTables()
+    {
+        $this->excludeDewdropTables = false;
+
+        return $this;
+    }
+
+    public function isWordPressCoreTable($table)
+    {
+        if (!$this->wordPressPrefix) {
+            throw new \Exception('WordPress prefix must be set before checking for core tables.');
+        }
+
+        $coreTableNames = [
+            'posts',
+            'comments',
+            'links',
+            'options',
+            'postmeta',
+            'terms',
+            'term_taxonomy',
+            'term_relationships',
+            'termmeta',
+            'commentmeta'
+        ];
+
+        foreach ($coreTableNames as $coreTableName) {
+            $pattern = sprintf(
+                '/^%s.*%s$/',
+                preg_quote($this->wordPressPrefix, '/'),
+                preg_quote($coreTableName, '/')
+            );
+
+            if (preg_match($pattern, $table)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
