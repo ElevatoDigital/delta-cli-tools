38397092c26f6f231942024caeed39f8696e9cb3
Darby Felton <darby.felton@deftek.com>
2017-11-14 12:26:03 -0500

    Add --chmod=Dg+s,ug+rw to default rsync flags

--- library/DeltaCli/Script/Step/Rsync.php
+++ library/DeltaCli/Script/Step/Rsync.php
@@ -63,7 +63,7 @@ class Rsync extends EnvironmentHostsStepAbstract implements DryRunInterface
     /**
      * @var string
      */
-    private $flags = '-az --no-p --no-t';
+    private $flags = '-az --no-p --no-t --chmod=Dg+s,ug+rw';
 
     /**
      * @var Slugify
