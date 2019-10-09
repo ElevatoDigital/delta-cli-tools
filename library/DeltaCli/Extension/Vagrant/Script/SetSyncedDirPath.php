bd1b7bfcdbe9f2b71c3343349e2b9c086ac9db58
Ceili Cornelison <ccornelison@deltasys.com>
2019-10-09 15:31:19 -0500

    Setting cached global synced-dir-path variable via command

--- library/DeltaCli/Extension/Vagrant/Script/SetSyncedDirPath.php
+++ library/DeltaCli/Extension/Vagrant/Script/SetSyncedDirPath.php
@@ -18,4 +18,33 @@ class SetSyncedDirPath extends Script
             'Set the path to your synced /delta directory if it is not in root.'
         );
     }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    protected function configure()
+    {
+        $this->addSetterArgument(
+            'path',
+            InputArgument::REQUIRED,
+            'The path to your synced /delta direcotry. This is the directory you will clone projects into, usually at root level.'
+        );
+
+        parent::configure();
+    }
+
+    protected function addSteps()
+    {
+        $this
+            ->addStep(
+                'set-synced-dir-path',
+                function () {
+                    $this->getProject()->getGlobalCache()->store('synced-dir-path', realpath($this->path));
+                }
+            );
+    }
 }
